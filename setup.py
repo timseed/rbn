e669d13dcb092a68da783db4b7b90111615f019f
tim <tim@sy-edm.com>
2016-03-16 18:27:09 +0400

    Added Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='RBN',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Public',
+    author='tim',
+    author_email='tim@sy-edm.com',
+    description='Reverse Beacon Network code'
+)
